f63a6a9b5995d166bf62c8a01c2e4e900f76493c
Hifumiii <hoangleminhminecraft@gmail.com>
2024-03-03 00:34:56 +0700

    Added Wall, Area classes for map rendering. working on level 1 and player entities

---
+++ src/Map.h
@@ -0,0 +1,49 @@
+#pragma once
+#include "Entity.h"
+#include <vector>
+
+
+class Wall : public Entity {
+    public:
+        Wall(Vector2f p_pos, SDL_Texture *p_tex) : Entity(p_pos, p_tex)
+        {}
+
+        bool checkCollisions(Entity &p_Entity) {
+            SDL_Rect temp = p_Entity.getCurrentFrame();
+            return SDL_HasIntersection(&temp, &currentFrame);
+        }
+
+    private:
+        Vector2f pos;
+        SDL_Texture *tex;
+        SDL_Rect currentFrame;
+};
+
+class Area {
+    public:
+        Area(std::vector<Wall> p_Walls, std::vector<Entity> p_Entities, bool p_valid)
+        :Walls(p_Walls), Entities(p_Entities), valid(p_valid)
+        {}
+        bool isValid() {
+            return valid;
+        }
+        bool checkCollisions(Entity &p_Entity) {
+            bool collision = false;
+            for (auto &it: Walls)
+            {
+                if (it.checkCollisions(p_Entity)) {
+                    collision = true;
+                    break;
+                }
+            }
+            return collision;
+        }
+        std::vector<Wall> getWalls() {
+            return Walls;
+        }
+
+    private:
+        std::vector<Wall> Walls;
+        std::vector<Entity> Entities;
+        bool valid;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include "RenderWindow.h"
 #include "Entity.h"
 #include "Math.h"
+#include "Map.h"
 
 bool init()
 {
@@ -44,7 +45,7 @@ SDL_Color white = {255, 255, 255};
 SDL_Color black = {0, 0, 0};
 
 int level = 0;
-int state = 0;
+int state = 1;
 
 bool gameRunning = true;
 
@@ -54,10 +55,15 @@ Uint64 currentTick = SDL_GetPerformanceCounter();
 Uint64 lastTick = 0;
 double deltaTime = 0;
 
+void loadMaps(std::vector<std::vector<std::vector<Area>>> &levels);
+void loadLevels(int &level);
+
 void game();
 void titleScreen();
 void update();
 void graphics();
+
+std::vector<std::vector<std::vector<Area>>> levels;
 
 int main(int argc, char* args[]) {
     std::cout << "Program started..." << std::endl;
@@ -66,7 +72,9 @@ int main(int argc, char* args[]) {
         std::cerr << "Initialization failed." << std::endl;
         return 1;
     }
-    
+
+    loadMaps(levels);
+
 	while (gameRunning)
 	{
 		game();
@@ -106,7 +114,7 @@ void titleScreen() {
 
     window.clear();
     window.render(0, 0, background_Texture);
-    
+
     window.render(155, 155, "Elemental Maze", font64, black);
 	window.render(150, 150, "Elemental Maze", font64, white);
 
@@ -114,9 +122,78 @@ void titleScreen() {
 }
 
 void update() {
-
+	while (SDL_PollEvent(&event))
+    {
+    	switch(event.type)
+    	{
+            case SDL_QUIT:
+                gameRunning = false;
+                break;
+    	}
+    }
 }
 
 void graphics() {
+	if (state == 1) 
+	{
+		window.clear();
+		window.render(0, 0, background_Texture);
+		for (size_t i = 0; i < levels[level].size(); ++i)	// Check each row of areas of each level
+		{
+			/* std::vector<std::vector<std::vector<Area>>> &levels; */
+			for (size_t j = 0; j < levels[level][i].size(); j++)	// Check each area of each row
+			{
+				if (levels[level][i][j].isValid())	// Check if that area exists
+				{
+					for (Wall &w : levels[level][i][j].getWalls())	// Render all walls in that area
+					{
+						window.render(w);
+					}
+					
+				}
+				
+			}
+			
+		}
+		
+		window.display();
+	}
+}
 
+
+
+void loadMaps(std::vector<std::vector<std::vector<Area>>> &levels) {
+    // Level 1
+    std::vector<std::vector<Area>> level1;
+    level1.push_back({});   // First row of Areas
+    level1[0].push_back(Area({      // Area [0][0]
+        Wall({64, 64}, brick_wall_Texture), 
+        Wall({96, 64}, brick_wall_Texture), 
+        Wall({128, 64}, brick_wall_Texture), 
+        Wall({160, 64}, brick_wall_Texture), 
+        Wall({192, 64}, brick_wall_Texture), 
+        Wall({224, 64}, brick_wall_Texture), 
+        Wall({256, 64}, brick_wall_Texture), 
+        Wall({288, 64}, brick_wall_Texture), 
+        Wall({320, 64}, brick_wall_Texture), 
+        Wall({352, 64}, brick_wall_Texture), 
+        Wall({384, 64}, brick_wall_Texture), 
+        Wall({416, 64}, brick_wall_Texture)
+    }, {
+        // vector of Entities
+    }, true));
+
+    levels.push_back(level1);
+}
+
+void loadLevel(int &level) {
+    switch (level)
+    {
+    case 0:
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
 }
