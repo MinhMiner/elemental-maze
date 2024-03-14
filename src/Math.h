09c1646a5328735f7da5902edc3086fc41b698a4
Hifumiii <hoangleminhminecraft@gmail.com>
2024-03-14 23:50:34 +0700

    added Effect structure for the new Effect system to replace the old one which used global variables

--- src/Math.h
+++ src/Math.h
@@ -2,6 +2,13 @@
 
 #include <iostream>
 
+enum effectType {
+    SPEED,
+    SHIELD,
+    DASH,
+    INVINCIBLE
+};
+
 struct Vector2f {
     Vector2f()
     :x(0.0f), y(0.0f)
@@ -18,3 +25,23 @@ struct Vector2f {
 
     float x, y;
 };
+
+struct Effect {
+    Effect()
+    :effectName(SPEED), amplifier(0.25f), duration(0.0f)
+    {}
+
+    Effect(effectType p_effectName, double p_amplifier, double p_duration)
+    :effectName(p_effectName), amplifier(p_amplifier), duration(p_duration)
+    {}
+
+    void print()
+	{
+		std::cout << effectName << ", " << amplifier << ", " << duration << std::endl;
+	}
+
+    effectType effectName;
+    double amplifier;
+    double duration;
+};
+
