b177888393e6d551aa3d8dfba0f2eb5ffba20394
Hifumiii <hoangleminhminecraft@gmail.com>
2024-04-10 22:05:57 +0700

    added basic methods for Button class

--- src/Button.h
+++ src/Button.h
@@ -6,15 +6,26 @@
 
 class Button {
     public:
-        Button(Vector2f p_pos, SDL_Texture *p_tex):pos(p_pos), tex(p_tex) {
-            is_hovered = false;
-            is_clicked = false;
+        Button(Vector2f p_pos, int p_w, int p_h, SDL_Texture *p_tex):pos(p_pos), tex(p_tex) {
+            hovered = false;
+            clicked = false;
+
+            currentFrame.x = 0;
+            currentFrame.y = 0;
+
+            SDL_QueryTexture(p_tex, NULL, NULL, &currentFrame.w, &currentFrame.h);
         }
         ~Button() {}
+        Vector2f getPos() { return pos; }
+        void setPos(Vector2f p_pos) { pos = p_pos; }
+        bool isHovered() { return isHovered; }
+        bool isClicked() { return isClicked; }
+        void update(const int &mouseX, const int &mouseY, const bool &keyMousePressed);
 
     private:
         Vector2f pos;
         SDL_Texture *tex;
-        bool is_hovered;
-        bool is_clicked;
+        SDL_Rect currentFrame;
+        bool hovered;
+        bool clicked;
 };
--- src/button.cpp
+++ src/button.cpp
@@ -1,1 +1,14 @@
 #include "Button.h"
+
+void Button::update(const int &mouseX, const int &mouseY, const bool &keyMousePressed)
+{
+    if (mouseX >= pos.x && mouseX <= pos.x + currentFrame.w &&
+        mouseY >= pos.y && mouseY <= pos.y + currentFrame.h) {
+        hovered = true;
+        if (keyMousePressed)
+            clicked = true;
+        else
+            clicked = false;
+    } else
+        hovered = false;
+}
